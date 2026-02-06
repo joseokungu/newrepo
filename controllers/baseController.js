d3441ab77970198210f06faeed991d6741ea3d50
joseokungu <joseokungu3@gmail.com>
2026-02-06 13:46:06 +0200

    uncomment

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,7 +3,6 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
-  // const nav = await utilities.getNav();
   res.render("index", {title: "Home", nav})
 }
 
