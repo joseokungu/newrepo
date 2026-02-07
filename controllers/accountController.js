d604ac90719428e3c5c99268923f7603240e6145
joseokungu <joseokungu3@gmail.com>
2026-02-07 14:47:46 +0200

    process of registration

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,6 +2,7 @@
 *  Account Controller
 * *************************************** */
 const utilities = require("../utilities/")
+const accountModel = require("../models/account-model")
 
 const accountCont = {}
 
@@ -38,6 +39,39 @@ accountCont.buildRegister = async function (req, res, next) {
     nav,
     errors: null,
   })
+}
+
+/* ****************************************
+*  Process Registration
+* *************************************** */
+accountCont.registerAccount = async function (req, res) {
+  let nav = await utilities.getNav()
+  const { account_firstname, account_lastname, account_email, account_password } = req.body
+
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  )
+
+  if (regResult) {
+    req.flash(
+      "notice",
+      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+    )
+    res.status(201).render("account/login", {
+      title: "Login",
+      nav,
+      errors: null,
+    })
+  } else {
+    req.flash("notice", "Sorry, the registration failed.")
+    res.status(501).render("account/register", {
+      title: "Registration",
+      nav,
+    })
+  }
 }
 
 
