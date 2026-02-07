e7b67a4c226b8f60d7eff732edbc4b223a062c2f
joseokungu <joseokungu3@gmail.com>
2026-02-07 10:02:46 +0200

    add account routes

--- server.js
+++ server.js
@@ -52,6 +52,8 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// Account routes
+app.use("/account", require("./routes/accountRoute"))
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
