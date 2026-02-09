0b556b2cd13029a71d16d9c0aba816de90f2b2bf
joseokungu <joseokungu3@gmail.com>
2026-02-09 14:02:38 +0200

    build routes just for admins or employees

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,11 +3,39 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities")
+const invValidate = require('../utilities/inventory-validation')
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
 // Route to build details by details view
 router.get("/detail/:invId", utilities.handleErrors(invController.buildByInventoryId));
+
+// Route to build the management view
+router.get("/", utilities.handleErrors(invController.buildManagement));
+
+//-- JUST FOR THE ADMINS OR EMPLOYEES --//
+
+// Route to build the add-classification view
+router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification));
+
+// Route to build the add-inventory view
+router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventory));
+
+// Process the add-classification data
+router.post(
+    "/add-classification",
+    invValidate.classificationRules(),
+    invValidate.checkClassificationData,
+    utilities.handleErrors(invController.AddClassificationName)
+)
+
+// Process the add-inventory data
+router.post(
+    "/add-inventory",
+    invValidate.inventoryRules(),
+    utilities.handleErrors(invController.AddInventory)
+)
+
 
 module.exports = router;
