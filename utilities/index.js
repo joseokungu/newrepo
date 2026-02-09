5d6c583a39ce3d67a26b505c6a28c02dfb1a3626
joseokungu <joseokungu3@gmail.com>
2026-02-09 14:03:42 +0200

    Build the the classification list for the add-inventory view

--- utilities/index.js
+++ utilities/index.js
@@ -118,5 +118,26 @@ Util.buildDetailsDisplay = async function (data) {
  **************************************** */
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
+/* **************************************
+* Build the the classification list for the add-inventory view
+* ************************************ */
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications()
+  let classificationList =
+    '<select name="classification_id" id="classificationList" required>'
+  classificationList += "<option value=''>Choose a Classification</option>"
+  data.rows.forEach((row) => {
+    classificationList += '<option value="' + row.classification_id + '"'
+    if (
+      classification_id != null &&
+      row.classification_id == classification_id
+    ) {
+      classificationList += " selected "
+    }
+    classificationList += ">" + row.classification_name + "</option>"
+  })
+  classificationList += "</select>"
+  return classificationList
+}
 
 module.exports = Util
