f672b4bae234e9e8082a532cc4b7959d7e3d6cfd
joseokungu <joseokungu3@gmail.com>
2026-02-09 14:00:03 +0200

    add new classification and inventory

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -42,4 +42,28 @@ async function getDetailsByInventoryId(inv_id) {
   }
 }
 
-module.exports = {getClassifications, getInventoryByClassificationId, getDetailsByInventoryId};
+/* *****************************
+*   Add New Classification
+* *************************** */
+async function AddNewClassification(classification_name){
+  try {
+    const sql = "INSERT INTO public.classification (classification_name) VALUES ($1) RETURNING *"
+    return await pool.query(sql, [classification_name])
+  } catch (error) {
+    return error.message
+  }
+}
+
+/* *****************************
+*   Add New Inventory
+* *************************** */
+async function AddNewInventory(classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color){
+  try {
+    const sql = "INSERT INTO public.inventory (classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9, $10) RETURNING *"
+    return await pool.query(sql, [classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color])
+  } catch (error) {
+    return error.message
+  }
+}
+
+module.exports = {getClassifications, getInventoryByClassificationId, getDetailsByInventoryId, AddNewClassification, AddNewInventory};
