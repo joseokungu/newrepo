32e6e464c9ba15a4da3cd132097a556e1c1abf77
joseokungu <joseokungu3@gmail.com>
2026-02-15 00:22:01 +0200

    format

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -83,4 +83,137 @@ validate.checkRegData = async (req, res, next) => {
   next()
 }
 
+/*  **********************************
+  *  Login Data Validation Rules
+  * ********************************* */
+validate.loginRules = () => {
+    return [
+        body("account_email")
+            .trim()
+            .notEmpty()
+            .withMessage("Please enter your email.")
+            .isEmail()
+            .withMessage("A valid email is required.")
+            .normalizeEmail(),
+
+        body("account_password")
+            .trim()
+            .notEmpty()
+            .withMessage("Please enter your password."),
+    ]
+}
+
+/* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkLoginData = async (req, res, next) => {
+    const { account_email } = req.body
+    let errors = []
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const nav = await utilities.getNav()
+        res.render("account/login", {
+            errors,
+            title: "Login",
+            nav,
+            account_email,
+        })
+        return
+    }
+    next()
+}
+
+/*  **********************************
+  *  Account Info Update Data Validation Rules
+  * ********************************* */
+validate.updateInfoRules = () => {
+    return [
+        // firstname is required and must be string
+        body("account_firstname")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 1 })
+            .withMessage("Please provide a valid first name."), // on error this message is sent.
+  
+        // lastname is required and must be string
+        body("account_lastname")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 2 })
+            .withMessage("Please provide a valid last name."), // on error this message is sent.
+  
+        // valid email is required and cannot already exist in the database
+        body("account_email")
+            .trim()
+            .isEmail()
+            .normalizeEmail() // refer to validator.js docs
+            .withMessage("A valid email is required.")
+    ]
+}
+
+/* ******************************
+ * Check data and return errors or continue to update info
+ * ***************************** */
+validate.checkUpdInfoData = async (req, res, next) => {
+    const { account_firstname, account_lastname, account_email, account_id } = req.body
+    let errors = []
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        let nav = await utilities.getNav()
+        res.render("account/update", {
+            errors,
+            title: "Update Account",
+            nav,
+            account_firstname,
+            account_lastname,
+            account_email,
+            account_id
+        })
+        return
+    }
+    next()
+}
+
+
+/*  **********************************
+  *  Password Update Data Validation Rules
+  * ********************************* */
+validate.passwordUpdateRules = () => {
+    return [  
+      // password is required and must be strong password
+        body("account_password")
+            .trim()
+            .notEmpty()
+            .isStrongPassword({
+                minLength: 12,
+                minLowercase: 1,
+                minUppercase: 1,
+                minNumbers: 1,
+                minSymbols: 1,
+            })
+            .withMessage("Password does not meet requirements."),
+        
+    ]
+}
+
+/* ******************************
+ * Check data and return errors or continue to update view
+ * ***************************** */
+validate.checkUpdtPasswordData = async (req, res, next) => {
+    let errors = []
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const nav = await utilities.getNav()
+        res.render("account/update", {
+            errors,
+            title: "Update Account",
+            nav,
+        })
+        return
+    }
+    next()
+}
+
 module.exports = validate
