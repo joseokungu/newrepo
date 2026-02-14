8b2a23714523758ce8396db191bd297ed8e033a6
joseokungu <joseokungu3@gmail.com>
2026-02-14 20:24:27 +0200

    checkInventoryData and checkUpdateData

--- utilities/inventory-validation.js
+++ utilities/inventory-validation.js
@@ -123,4 +123,67 @@ validate.checkClassificationData = async (req, res, next) => {
   next()
 }
 
+/* ******************************
+ * Check data and return errors or continue to add new inventory
+ * ***************************** */
+validate.checkInventoryData = async (req, res, next) => {
+  const { classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color } = req.body;
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    let classificationList = await utilities.buildClassificationList(classification_id);
+    res.render("inventory/add-inventory", {
+      errors,
+      title: "Add New Inventory",
+      nav,
+      classificationList,
+      inv_make,
+      inv_model,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_year,
+      inv_miles,
+      inv_color,
+    })
+    return
+  }
+  next()
+}
+
+/* ******************************
+ * Check data and return errors or continue to edit inventory
+ * ***************************** */
+validate.checkUpdateData = async (req, res, next) => {
+  const { inv_id, classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color } = req.body;
+  const itemName = `${inv_make} ${inv_model}`
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    let classificationList = await utilities.buildClassificationList(classification_id);
+    res.render("inventory/edit-inventory", {
+      errors,
+      title: "Edit " + itemName,
+      nav,
+      inv_id,
+      classificationList,
+      inv_make,
+      inv_model,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_year,
+      inv_miles,
+      inv_color,
+    })
+    return
+  }
+  next()
+}
+
+
 module.exports = validate
